b491c8c63ceb395e5a801fe33f8153896b7c02b1
Your Name <you@example.com>
2022-11-07 13:55:07 +0300

    second commit

--- src/workshop/api/operations.py
+++ src/workshop/api/operations.py
@@ -1,13 +1,23 @@
-from fastapi import APIRouter
+from typing import List
 
+from fastapi import APIRouter
+from .. import tables
+from ..models.operations import Operation
+from ..database import Session
 
 router = APIRouter(
     prefix='/operations'
 )
 
 
-@router.get('/')
+@router.get('/', response_model=List[Operation])
 def get_operations():
-    return []
+    session = Session()
+    operations = (
+        session
+        .query(tables.Operation)
+        .all()
+    )
+    return operations
 
 
--- src/workshop/models/operations.py
+++ src/workshop/models/operations.py
@@ -1,13 +1,17 @@
-from  pydantic import BaseModel
+from pydantic import BaseModel
 from datetime import date
 from enum import Enum
 from typing import Optional
 from decimal import Decimal
 
+
+class OperationKind(str, Enum):
+    INCOME = 'income'
+    OUTCOME = 'outcome'
 class Operation(BaseModel):
     id: int
     date: date
-    kind: E
+    kind: OperationKind
     amount: Decimal
     description: Optional[str]
 
